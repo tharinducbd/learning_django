ddce4ef98998138aef20feb54b4051f9145d171e
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-05-04 06:34:50 +0530

    DATABASES, env

--- diary/diary/settings.py
+++ diary/diary/settings.py
@@ -1,10 +1,15 @@
 from pathlib import Path
 
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=h(dyxlr43cx6r@%4ig$8grq(z0&q@m9&k@eh#=&q#y3+8w=p@'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -59,8 +64,12 @@ WSGI_APPLICATION = 'diary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
     }
 }
 
