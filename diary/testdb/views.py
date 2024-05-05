5f50391a6cd5b0f71d613cbb4db7ff6b65fa0e8a
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-05-05 06:36:09 +0530

    CBV: index >> IndexView

--- diary/testdb/views.py
+++ diary/testdb/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from typing import Any
+from django.db.models.query import QuerySet
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_list_or_404 ,render
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hi!")
+from .models import Teacher
+
+
+class IndexView(generic.ListView):
+    template_name = "testdb/index.html"
+    context_object_name = "list_of_teachers"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        """Return the list of teachers"""
+        return Teacher.objects.all().order_by("-age")
