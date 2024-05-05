4810c680c32dafcbad6d4116dcecefad6952b6b0
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-05-05 06:36:40 +0530

    __str__ for Teacher

--- diary/testdb/models.py
+++ diary/testdb/models.py
@@ -5,3 +5,10 @@ class Teacher(models.Model):
     name = models.CharField(max_length=80)
     age = models.IntegerField()
     subject = models.CharField(max_length=80)
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.subject}"
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=80)
