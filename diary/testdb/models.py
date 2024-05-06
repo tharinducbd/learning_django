2d13af9b477657811e06a8f4bd53bb22b89b0dae
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-05-06 07:28:37 +0530

    Teacher updated, Meta added

--- diary/testdb/models.py
+++ diary/testdb/models.py
@@ -1,23 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
 class Subject(models.Model):
     name = models.CharField(max_length=80)
 
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.name} (Subject id: {self.id})"
+    
+    class Meta:
+        ordering = ["name"]
+        # verbose_name_plural = "Subjects"
 
 
 class Teacher(models.Model):
     name = models.CharField(max_length=80)
     age = models.IntegerField()
-    subject = models.CharField(max_length=80)
-    # subject_link = models.ForeignKey(Subject,
-    #                                  on_delete=models.CASCADE,
-    #                                  related_name="Teachers")
+    subject = models.ManyToManyField(Subject)
 
     def __str__(self) -> str:
-        return f"{self.name} - {self.subject}"
+        return f"{self.name} ({self.age})"
+    
+    class Meta:
+        ordering = ["name", "age"]
+        # verbose_name_plural = "Teachers"
 
 
 class Student(models.Model):
