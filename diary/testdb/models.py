98cf77fbd9de4cc0f23c599031b1da69894fec95
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-05-06 05:58:50 +0530

    May 6th: Model Trials commence

--- diary/testdb/models.py
+++ diary/testdb/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class Teacher(models.Model):
     name = models.CharField(max_length=80)
     age = models.IntegerField()
     subject = models.CharField(max_length=80)
+    # subject_link = models.ForeignKey(Subject,
+    #                                  on_delete=models.CASCADE,
+    #                                  related_name="Teachers")
 
     def __str__(self) -> str:
         return f"{self.name} - {self.subject}"
