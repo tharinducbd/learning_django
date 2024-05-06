0f83dbad94c2604ac2bedb5e0547e7613771d2a1
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-05-06 07:43:27 +0530

    House, Student registered

--- diary/testdb/admin.py
+++ diary/testdb/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Subject, Teacher
+from .models import House, Student, Subject, Teacher
 
 
+admin.site.register(House)
+admin.site.register(Student)
 admin.site.register(Subject)
 admin.site.register(Teacher)
