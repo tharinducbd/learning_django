d003b4705c0f4e5b5420fba67d0bbce162284207
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-05-07 17:30:47 +0530

    Registered House, Student, Subject, Teacher

--- hogwarts/administration/admin.py
+++ hogwarts/administration/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import House, Student, Subject, Teacher
+
+admin.site.register(House)
+admin.site.register(Student)
+admin.site.register(Subject)
+admin.site.register(Teacher)
