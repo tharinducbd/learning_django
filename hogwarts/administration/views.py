757ee6e06255be7d97a018f53acdbe3a84681ee0
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-05-07 18:58:55 +0530

    basic list views for all object types

--- hogwarts/administration/views.py
+++ hogwarts/administration/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import House, Student, Subject, Teacher
 
 
-# Create your views here.
-def index(self):
-    return HttpResponse("Hogwarts welcomes you!")
+def index(request):
+    houses = House.objects.all()
+    teachers = Teacher.objects.all()
+    subjects = Subject.objects.all()
+    students = Student.objects.all()
+
+    context = {
+        "all_houses": houses,
+        "all_teachers": teachers,
+        "all_subjects": subjects,
+        "all_students": students,
+    }
+
+    return render(request, "administration/index.html", context)
