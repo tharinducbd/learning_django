c0f396daa524e90a54a107cad8ee041bb5a96bca
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-05-14 18:50:02 +0530

    ModelChoiceField and ModelMultipleChoiceField

--- hogwarts/administration/forms.py
+++ hogwarts/administration/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 
+from .models import House, Student, Subject, Teacher
+
 
 class TestForm(forms.Form):
-    name = forms.CharField(label="Full Name")
-    house = forms.CharField(label="House")
+    name = forms.CharField(label="Full Name",)
+    house = forms.ModelChoiceField(queryset=House.objects.all())
+    subjects = forms.ModelMultipleChoiceField(queryset=Subject.objects.all())
