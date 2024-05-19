206a380cdb3b546e6168463e3a5152e3aebc49ce
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-05-19 17:39:53 +0530

    Custom fields for house and subjects

--- hogwarts/administration/forms.py
+++ hogwarts/administration/forms.py
@@ -1,9 +1,25 @@
 from django import forms
+from django.db.models.base import Model
 
 from .models import House, Student, Subject, Teacher
 
 
+class HouseModelChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj: Model) -> str:
+        return f"{obj.name} ({obj.emblem})"
+
+
+class SubjectMultipleModelChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj: Model) -> str:
+        return f"{obj.name} {obj.id}"
+
+
 class AddStudentForm(forms.Form):
     name = forms.CharField(label="Full Name",)
-    house = forms.ModelChoiceField(queryset=House.objects.all())
-    subjects = forms.ModelMultipleChoiceField(queryset=Subject.objects.all())
+    house = HouseModelChoiceField(queryset=House.objects.all(),
+                                   empty_label="Select the House",
+                                   widget=forms.RadioSelect,
+                                   )
+    subjects = SubjectMultipleModelChoiceField(queryset=Subject.objects.all(),
+                                               widget=forms.RadioSelect,
+                                               )
