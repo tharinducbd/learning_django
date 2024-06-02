3b3f8a726dde659ae3ad02ae201b40ffb72bb328
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-06-02 19:21:45 +0530

    basic urls and templates

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -21,4 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("pages.urls")),
+    path("projects/", include("projects.urls")),
 ]
---
+++ portfolio/projects/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from projects import views
+
+
+app_name = 'projects'
+urlpatterns = [
+    path("", views.project_index, name="project_index"),
+    path("<int:pk>/", views.project_detail, name="project_detail"),
+]
