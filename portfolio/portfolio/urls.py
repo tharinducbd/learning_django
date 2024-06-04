b47e79b7d07762b00fa8e6dcf2d600575374985d
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-06-04 05:58:48 +0530

    included users

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("pages.urls")),
     path("projects/", include("projects.urls")),
+    path("users/", include("users.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
