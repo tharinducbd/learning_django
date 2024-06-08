f3610d1da4ed9b30d5d237c88ae98e7f88a68323
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-06-08 07:17:11 +0530

    pass_change_done bug fix.. but explore!

--- portfolio/users/urls.py
+++ portfolio/users/urls.py
@@ -2,9 +2,15 @@ from django.urls import include, path, re_path
 
 from users.views import dashboard
 
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
+
 
 app_name = "users"
 urlpatterns = [
     re_path(r"^accounts/", include("django.contrib.auth.urls")),
+    path("password_change",
+         auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('users:password_change_done')),
+         name="password_change"),
     re_path(r"^dashboard/", dashboard, name="dashboard"),
 ]
