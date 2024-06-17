9491d9ee1bae30ef4a5bd9c8b64b7bb4b53e881b
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-06-17 06:02:46 +0530

    June 17th: message update

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("Welcome to the intermediate level of Django!")
+messegas = [
+    "Welcome to the intermediate level of Django!",
+    "The Server-side MDN Tutorial was added to the mix."
+]
+
+for msg in messegas:
+    print(msg)
