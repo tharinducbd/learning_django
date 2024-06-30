16abec008cc5ed2ae9da83638bdd2c5de184ed6e
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-06-30 17:09:16 +0530

    June 30th

--- library/catalog/models.py
+++ library/catalog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.urls import reverse     # required for get_absolute_url()
 
-# Create your models here.
+from django.db.models import UniqueConstraint   # Constrain fields to unique values
+from django.db.models.functions import Lower    # Returns lower cased value of field
+
+
+
