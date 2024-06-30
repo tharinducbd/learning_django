a9908ea56a6c24c08f5657e1c41f5934e79311ba
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-06-30 17:20:42 +0530

    genre model added

--- library/catalog/models.py
+++ library/catalog/models.py
@@ -5,4 +5,27 @@ from django.db.models import UniqueConstraint   # Constrain fields to unique val
 from django.db.models.functions import Lower    # Returns lower cased value of field
 
 
+class Genre(models.Model):
+    """Model representing a book genre."""
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)"
+    )
 
+    def __str__(self) -> str:
+        """String for representing the Model object."""
+        return self.name
+    
+    def get_absolute_url(self):
+        """Returns the url to access a particular genre instance."""
+        return reverse('genre-detail', args=[str(self.id)])
+    
+    class Meta:
+        constrains = [
+            UniqueConstraint(
+                Lower('name'),
+                name='genre_name_case_insensitive_unique',
+                violation_error_message="Genre already exists (case insensitive match)"
+            ),
+        ]
