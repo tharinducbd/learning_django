f4f3eb3105c345999fa2eda3de8a41893e922221
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-07-02 06:11:03 +0530

    Book model v1

--- library/catalog/models.py
+++ library/catalog/models.py
@@ -31,5 +31,34 @@ class Genre(models.Model):
         ]
 
 
-    class Book(models.Model):
-        pass
+class Book(models.Model):
+    """Model representing a book (but not specific copy of a book)."""
+    title = models.CharField(max_length=200)
+
+    author = models.ForeignKey('Author', on_delete=models.RESTRICT, null=True)
+    # Author as a string rather than an object, because it hasn't been declared yet
+
+    summary = models.TextField(
+        max_length=1000,
+        help_text="Enter a brief description of the book",
+    )
+    isbn = models.CharField(
+        'ISBN',
+        max_length=13,
+        unique=True,
+        help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
+                  '">ISBN number</a>',
+    )
+
+    genre = models.ManyToManyField(
+        Genre,
+        help_text="Select a genre for this book",
+    )
+    # ManyToMany because each genre can have many books and a book can have multiple genres
+
+    def __str__(self) -> str:
+        return self.title
+
+    def get_absolute_url(self):
+        """Returns the URL to access a detail record for this book."""
+        return reverse('book-detail', args=[str(self.id)])
