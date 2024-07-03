6b36091a0f8eda9233a7f67ab60a7946b8734555
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-07-03 05:52:51 +0530

    BookInstance model added

--- library/catalog/models.py
+++ library/catalog/models.py
@@ -4,6 +4,8 @@ from django.urls import reverse     # required for get_absolute_url()
 from django.db.models import UniqueConstraint   # Constrain fields to unique values
 from django.db.models.functions import Lower    # Returns lower cased value of field
 
+import uuid     # Required for unique book instances
+
 
 class Genre(models.Model):
     """Model representing a book genre."""
@@ -57,6 +59,7 @@ class Book(models.Model):
     # ManyToMany because each genre can have many books and a book can have multiple genres
 
     def __str__(self) -> str:
+        """String for representing the Model object."""
         return self.title
 
     def get_absolute_url(self):
@@ -65,4 +68,32 @@ class Book(models.Model):
 
 
 class BookInstance(models.Model):
-    pass
+    """Model representing a specific copy of a book (i.e. that can be 
+    borrowed from the library)"""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this particular book across whole library")
+    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability',
+    )
+
+    class Meta:
+        ordering = ['due_back']
+    
+    def __str__(self) -> str:
+        """String for representing the Model object."""
+        return f'{self.id} ({self.book.title})'
