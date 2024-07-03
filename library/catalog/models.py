9a1d8b286b910183d637c7976efea0e74c1b03da
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-07-03 05:40:25 +0530

    July 3rd

--- library/catalog/models.py
+++ library/catalog/models.py
@@ -62,3 +62,7 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the URL to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+
+
+class BookInstance(models.Model):
+    pass
