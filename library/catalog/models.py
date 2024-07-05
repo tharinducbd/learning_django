8f6067a2f693139cd4518163f91ad2c36ea3c3d6
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-07-05 05:55:10 +0530

    style formatting

--- library/catalog/models.py
+++ library/catalog/models.py
@@ -12,7 +12,7 @@ class Genre(models.Model):
     name = models.CharField(
         max_length=200,
         unique=True,
-        help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)"
+        help_text="Enter a book genre (e.g. Fantasy, French Poetry etc.)"
     )
 
     def __str__(self) -> str:
@@ -62,7 +62,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
 
     author = models.ForeignKey('Author', on_delete=models.RESTRICT, null=True)
-    # Author as a string rather than an object, because it hasn't been declared yet
+    # Author as a string, not an object, because it hasn't been declared yet
 
     summary = models.TextField(
         max_length=1000,
@@ -72,15 +72,14 @@ class Book(models.Model):
         'ISBN',
         max_length=13,
         unique=True,
-        help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
-                  '">ISBN number</a>',
+        help_text='13 Character <a href="https://www.isbn-international.org/'
+                  'content/what-isbn">ISBN number</a>',
     )
 
     genre = models.ManyToManyField(
         Genre,
         help_text="Select a genre for this book",
-    )
-    # ManyToMany because each genre can have many books and a book can have multiple genres
+    )  # each genre can have many books and a book can have multiple genres
 
     def __str__(self) -> str:
         """String for representing the Model object."""
@@ -95,9 +94,12 @@ class BookInstance(models.Model):
     """Model representing a specific copy of a book
     (i.e. that can be borrowed from the library)"""
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
-                          help_text="Unique ID for this particular book across whole library")
-    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4,
+        help_text="Unique ID for this particular book across whole library"
+    )
+
+    book = models.ForeignKey(Book, on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
 
