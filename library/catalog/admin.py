c58cfa7bf176bf281288b67959978d012ec9f14f
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-07-07 06:11:45 +0530

    BookAdmin added with list_display

--- library/catalog/admin.py
+++ library/catalog/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 
 from .models import Author, Book, BookInstance, Genre, Language
 
+
+class BookAdmin(admin.ModelAdmin):
+    """Customizations for the Model objects list view."""
+    list_display = ["title", "author", "isbn"]
+
+
 admin.site.register(Author)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 admin.site.register(BookInstance)
 admin.site.register(Genre)
 admin.site.register(Language)
