ef997b9e4bd93fd0236345afda7f5956a83aed1a
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-07-13 06:28:34 +0530

    Index view: V1

--- library/catalog/views.py
+++ library/catalog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Author, Book, BookInstance, Genre
+
+
+def index(request):
+    """View function for hompe page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    num_authors = Author.objects.count()
+
+    context = {
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "num_authors": num_authors,
+    }
+
+    return render(request, "catalog/index.html", context)
