d1c8fa5bf476c9364823988cdd4f87fe6e78ce0a
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-07-23 05:37:08 +0530

    authors url path added

--- library/catalog/urls.py
+++ library/catalog/urls.py
@@ -7,5 +7,6 @@ app_name = 'catalog'
 urlpatterns = [
     path('', views.index, name="index"),
     path('books/', views.BookListView.as_view(), name='books'),
-    path('books/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
 ]
