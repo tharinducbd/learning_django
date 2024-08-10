33798923071ad3b6fed4f33cf783b4bed1fa164e
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-08-10 06:08:54 +0530

    BookInst: permissions defined

--- library/catalog/models.py
+++ library/catalog/models.py
@@ -145,6 +145,7 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ['book', 'due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self) -> str:
         """String for representing the Model object."""
