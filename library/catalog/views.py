456ab1ad316de328aed3cfa70159bc2de54ff8f1
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-08-20 05:36:53 +0530

    book renew decorator updated

--- library/catalog/views.py
+++ library/catalog/views.py
@@ -110,7 +110,7 @@ class LoanedBooksByAllUsersListView(PermissionRequiredMixin,generic.ListView):
 
 
 @login_required
-@permission_required('catalog.can_renew')
+@permission_required('catalog.can_renew', raise_exception=True)
 def renew_book_librarian(request, pk):
     """View function for renewing a specific BookInstance by librarians."""
     book_instance = get_object_or_404(BookInstance, id=pk)
