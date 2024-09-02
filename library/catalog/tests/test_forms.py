c9e75ca64d5cfb4149e5c4b346c284871a2fe819
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-09-02 06:10:29 +0530

    RnewBookFormTests: label, help_text

--- library/catalog/tests/test_forms.py
+++ library/catalog/tests/test_forms.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from catalog.forms import RenewBookForm
+
+
+class RenewBookFormTests(TestCase):
+    def test_renew_form_date_field_label(self):
+        form = RenewBookForm()
+        self.assertTrue(
+            form.fields['renewal_date'].label is None or form.fields['renewal_date'].label == 'renewal date'
+        )
+
+    def test_renew_form_date_field_help_text(self):
+        form = RenewBookForm()
+        self.assertIn(
+            'a date between now and 4 weeks', form.fields['renewal_date'].help_text
+        )
