e75aa19e39294a1baa0536662435cd07c27ce989
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-09-03 05:12:24 +0530

    RenewBookTFs: date logic tests(4)

--- library/catalog/tests/test_forms.py
+++ library/catalog/tests/test_forms.py
@@ -18,3 +18,23 @@ class RenewBookFormTests(TestCase):
         self.assertIn(
             'a date between now and 4 weeks', form.fields['renewal_date'].help_text
         )
+
+    def test_renew_form_date_in_past(self):
+        date = datetime.datetime.today() - datetime.timedelta(days=1)
+        form = RenewBookForm(data={'renewal_date': date})
+        self.assertFalse(form.is_valid())
+
+    def test_renew_form_date_too_far_in_future(self):
+        date = datetime.datetime.today() + datetime.timedelta(weeks=4) + datetime.timedelta(days=1)
+        form = RenewBookForm(data={'renewal_date': date})
+        self.assertFalse(form.is_valid())
+
+    def test_renew_form_date_today(self):
+        date = datetime.datetime.today()
+        form = RenewBookForm(data={'renewal_date': date})
+        self.assertTrue(form.is_valid())
+
+    def test_renew_form_date_max(self):
+        date = datetime.datetime.today() + datetime.timedelta(weeks=4)
+        form = RenewBookForm(data={'renewal_date': date})
+        self.assertTrue(form.is_valid())
