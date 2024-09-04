cd238d65b32c0218eb42ba3270ae89bdb96a9a8d
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-09-04 06:10:11 +0530

    URL tests for AuthorLVTs

--- library/catalog/tests/test_views.py
+++ library/catalog/tests/test_views.py
@@ -15,3 +15,11 @@ class AuthorListViewTests(TestCase):
                 first_name=f"Dominique {author_id}",
                 last_name=f"Surname {author_id}",
             )
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/catalog/authors/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('catalog:authors'))
+        self.assertEqual(response.status_code, 200)
