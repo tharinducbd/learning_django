f12cdfb49565ae5aee5b8700342dca2b7e4cf9c1
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-09-04 05:50:21 +0530

    AuthorLVTs: set up 12 authors

--- library/catalog/tests/test_views.py
+++ library/catalog/tests/test_views.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from catalog.models import Author
+
+
+class AuthorListViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        # Create 12 authors for pagination tests
+        num_authors = 12
+
+        for author_id in range(num_authors):
+            Author.objects.create(
+                first_name=f"Dominique {author_id}",
+                last_name=f"Surname {author_id}",
+            )
