3ae2eb7e9f6a2328f9e3a523f4bd6885b72ef3e7
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2024-09-13 06:52:34 +0530

    db env configuration added

--- library/library/settings.py
+++ library/library/settings.py
@@ -114,3 +114,12 @@ LOGIN_REDIRECT_URL = '/'
 
 # To log emails to console, to copy the password reset link from console
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Update database configuration from $DATABASE_URL environment variable (if defined)
+import dj_database_url
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=500,
+        conn_health_checks=True,
+    )
